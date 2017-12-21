a2187e0fc45ef46565e3a471e89ce6f534cfa966
Aurore <aurorem@theodo.fr>
2017-12-21 12:07:26 +0100

    Install react-native-sms 📱✉️

--- android/app/src/main/java/com/nholemobileapp/MainActivity.java
+++ android/app/src/main/java/com/nholemobileapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nholemobileapp;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
+import com.tkporter.sendsms.SendSMSPackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "nholeMobileApp";
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        SendSMSPackage.getInstance().onActivityResult(requestCode, resultCode, data);
     }
 }
