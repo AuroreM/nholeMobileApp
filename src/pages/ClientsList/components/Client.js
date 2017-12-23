1de48b19e7eb8f41685f27c7600868d945a54182
Aurore <aurorem@theodo.fr>
2017-12-23 09:47:35 +0100

    Design Client view 🎨

--- src/pages/ClientsList/components/Client.js
+++ src/pages/ClientsList/components/Client.js
@@ -1,32 +1,59 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 class Client extends React.Component {
+  styles = getStyles();
+
+  onDelete = () => this.props.deleteClient(this.props.client.item.id);
+
   render() {
     return (
       <View>
-        <View style={{ position: 'relative' }}>
-          <Text>
-            {this.props.client.item.firstname} {this.props.client.item.lastname}{' '}
+        <View style={this.styles.nameContainer}>
+          <Text style={this.styles.name}>
+            {this.props.client.item.firstname} {this.props.client.item.lastname}
           </Text>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.deleteClient(this.props.client.item.id);
-            }}
-            title="X"
-            style={{ height: 20, width: 20, backgroundColor: 'red' }}
-          />
+          <TouchableOpacity onPress={this.onDelete}>
+            <Icon name="close" size={24} style={{ color: 'rgb(220, 20, 60)' }} />
+          </TouchableOpacity>
         </View>
-        <Text>N° de téléphone : {this.props.client.item.number}</Text>
-        <Text>
-          Créneaux : {this.props.client.item.morning ? 'Matin, ' : ''}
-          {this.props.client.item.lunch ? 'Midi, ' : ''}
-          {this.props.client.item.afternoon ? 'Après-midi, ' : ''}
-          {this.props.client.item.evening ? 'Soirée, ' : ''}{' '}
-        </Text>
+        <View style={this.styles.infoContainer}>
+          <Text>N° de téléphone : </Text>
+          <Text>{this.props.client.item.number}</Text>
+        </View>
+        <View style={this.styles.infoContainer}>
+          <Text>Créneaux : </Text>
+          <Text style={this.styles.info}>
+            {this.props.client.item.morning ? 'Matin, ' : ''}
+            {this.props.client.item.lunch ? 'Midi, ' : ''}
+            {this.props.client.item.afternoon ? 'Après-midi, ' : ''}
+            {this.props.client.item.evening ? 'Soirée, ' : ''}{' '}
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
+const getStyles = () =>
+  StyleSheet.create({
+    nameContainer: {
+      flexDirection: 'row',
+      flex: 1,
+      justifyContent: 'space-between',
+      marginVertical: 5,
+    },
+    name: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    infoContainer: {
+      flexDirection: 'row',
+    },
+    info: {
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+  });
 export default Client;
